59ad50e51f1d4e3f29a54b44063eaf09215f93fa
Garin Wally <garwall101@gmail.com>
2016-09-13 15:16:11 -0600

    Fix density calc

--- tools/archive/other.sql
+++ tools/archive/other.sql
@@ -5,24 +5,27 @@ INSERT INTO all_multidev
   SELECT name, sum(sum_dwellings) FROM (
 
     -- Copy/paste this for individual projects
-    SELECT d.address AS address, d.sum_dwellings AS sum_dwellings, n.name AS name 
+    SELECT d.address AS address, sum(d.sum_dwellings) AS sum_dwellings, n.name AS name 
     FROM density2014 d 
     JOIN council_dists n ON Intersects(d.geometry, n.geometry) 
     WHERE d.sum_dwellings >= 3 
+	GROUP BY d.address
 
     UNION 
 
-    SELECT d.address AS address, d.sum_dwellings AS sum_dwellings, n.name AS name 
+    SELECT d.address AS address, sum(d.sum_dwellings) AS sum_dwellings, n.name AS name 
     FROM density2015 d 
     JOIN council_dists n ON Intersects(d.geometry, n.geometry) 
     WHERE d.sum_dwellings >= 3 
+	GROUP BY d.address
 
     UNION 
 
-    SELECT d.address AS address, d.sum_dwellings AS sum_dwellings, n.name AS name 
+    SELECT d.address AS address, sum(d.sum_dwellings) AS sum_dwellings, n.name AS name 
     FROM density2016 d 
     JOIN council_dists n ON Intersects(d.geometry, n.geometry) 
     WHERE d.sum_dwellings >= 3 
+	GROUP BY d.address
 
     ORDER BY n.name
 ) 
